c25f534ddbc379a8a5b1dfa5746edf7175470775
madhavhugar <madhav.hugar@gmail.com>
2020-05-02 04:48:30 +0200

    Update function to fetch non-lexical words

--- src/app/lexical-analysis/getLexicalWordCount.ts
+++ src/app/lexical-analysis/getLexicalWordCount.ts
@@ -1,7 +1,7 @@
-import getNonLexicalWords from '../models/getNonLexicalWords';
+import { getNonLexicalWordsFromLocal } from '../models/getNonLexicalWords';
 
 export default function getLexicalWordCount(words: string[]): number {
-  const nonLexicalWords = getNonLexicalWords();
+  const nonLexicalWords = getNonLexicalWordsFromLocal();
   return words
     .filter(word => !nonLexicalWords.includes(word))
     .length;

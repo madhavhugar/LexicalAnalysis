7cf7717bf284d46d31bf3d91571232e73c05860f
madhavhugar <madhav.hugar@gmail.com>
2020-05-02 04:41:10 +0200

    Remove index controller

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,5 +1,4 @@
 import { json } from 'express';
-import { indexRouter } from './controllers/index.controller';
 import { lexicalRouter } from './lexical-analysis';
 import { errorHandler } from './middleware/error-handler';
 import swagger from 'swagger-ui-express';
@@ -7,8 +6,7 @@ const swaggerDocument = require('./swagger.json');
 
 export function initRoutes(app: any) {
     app.use(json());
-    app.use('/', indexRouter);
     app.use('/complexity', lexicalRouter);
-    app.use('/api-docs', swagger.serve, swagger.setup(swaggerDocument));
+    app.use('/', swagger.serve, swagger.setup(swaggerDocument));
     app.use(errorHandler);
 }
